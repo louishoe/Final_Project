76d4499562808b5b48fc85ec17f7a792320c6440
jillian-green <jillian_green@brown.edu>
2021-12-11 17:19:19 +0000

    updated map

--- src/map.py
+++ src/map.py
@@ -16,6 +16,9 @@ app = dash.Dash(__name__)
 gs = 'gs://live.csv/'
 df = pd.read_csv(gs+'data_file.csv')
 
+#conduct grouping:
+
+
 #set up your components and html needed
 app.layout = html.Div([
     html.H1("Weather Map (Test)", style={'text-align': 'center'}),
@@ -23,10 +26,70 @@ app.layout = html.Div([
     dcc.Dropdown(id="Select_Year",
                 options=[
                     {"label": "2016", "value": 2016},
-                    {"label": "2016", "value": 2017},
-                    {"label": "2016", "value": 2018},
-                    {"label": "2016", "value": 2019},
-                    {"label": "2016", "value": 2020},
-                    {"label": "2016", "value": 2021}
-                ])
+                    {"label": "2017", "value": 2017},
+                    {"label": "2018", "value": 2018},
+                    {"label": "2019", "value": 2019},
+                    {"label": "2020", "value": 2020},
+                    {"label": "2021", "value": 2021}],
+                multi=False,
+                value=2015,
+                style={'width': '40%'}
+                ),
+    html.Div(id= 'output_container', children= []),
+    html.Br(), 
+    dcc.Graph(id='AQ_Map', figure= {})
 ])
+
+# Connect the Plotly graphs with Dash Components
+@app.callback(
+    [Output(component_id='output_container', component_property='children'),
+     Output(component_id='AQ_Map', component_property='figure')],
+    [Input(component_id='Select_Year', component_property='value')]
+)
+def update_graph(option_slctd):
+    print(option_slctd)
+    print(type(option_slctd))
+
+    container = "The year chosen by user was: {}".format(option_slctd)
+
+    dff = df.copy()
+    dff = dff[dff["Year"] == option_slctd]
+    dff = dff[dff["Affected by"] == "Varroa_mites"]
+
+    # Plotly Express
+    fig = px.choropleth(
+        data_frame=dff,
+        locationmode='USA-states',
+        locations='state_code',
+        scope="usa",
+        color='Pct of Colonies Impacted',
+        hover_data=['State', 'Pct of Colonies Impacted'],
+        color_continuous_scale=px.colors.sequential.YlOrRd,
+        labels={'Pct of Colonies Impacted': '% of Bee Colonies'},
+        template='plotly_dark'
+    )
+
+    # Plotly Graph Objects (GO)
+    # fig = go.Figure(
+    #     data=[go.Choropleth(
+    #         locationmode='USA-states',
+    #         locations=dff['state_code'],
+    #         z=dff["Pct of Colonies Impacted"].astype(float),
+    #         colorscale='Reds',
+    #     )]
+    # )
+    #
+    # fig.update_layout(
+    #     title_text="Bees Affected by Mites in the USA",
+    #     title_xanchor="center",
+    #     title_font=dict(size=24),
+    #     title_x=0.5,
+    #     geo=dict(scope='usa'),
+    # )
+
+    return container, fig
+
+
+# ------------------------------------------------------------------------------
+if __name__ == '__main__':
+    app.run_server(debug=True)
