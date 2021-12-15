c08525b347251b101d1bcd3b53fb7bc8de3e4abc
michael-tap-ia <michael_tapia@brown.edu>
2021-12-15 19:57:17 +0000

    updated

--- app.py
+++ app.py
@@ -41,15 +41,20 @@ def description():
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
-        html.P("Current Weather in the United States:"),
+        html.P("Weather and Pollution Analysis in the United States by State:"),
         dcc.Dropdown(
         id='states', 
         options=[{'value': x, 'label': x} 
                  for x in live['state']],
         value="Rhode Island"),
-    dcc.Graph(id="USA_MAP",style={'display': 'inline-block'}),
-    dcc.Graph(id="bar_line", style={'display': 'inline-block'})
-    #display_choropleth(live)
+        dcc.Graph(id='gen_metrics_temp', style={'display': 'inline-block'} ),
+        dcc.Graph(id='gen_metrics_feels', style={'display': 'inline-block'} ),
+
+        dcc.Graph(id="USA_MAP",style={'display': 'inline-block'}),
+        dcc.Graph(id="bar_line", style={'display': 'inline-block'}),
+        dcc.Graph(id="Weahter_forecast", style={'display': 'inline-block'})
+        #dcc.Graph(id="AQI_Hist_line")
+      
 ])
 
 @app.callback(
@@ -79,13 +84,13 @@ def display_graph(states):
                     )
     line_graph = go.Line(x=df['graph_date'],
                         y=df['Temperature'],
-                        name='Temperature',
+                        name='Temperature (°F)',
                         mode='lines+markers',
                         yaxis='y2')
 
     data = [line_graph, bar_graph]
 
-    layout = go.Layout(title={'text': 'Temperature and Pollution',
+    layout = go.Layout(title={'text': '{state}\'s Hourly Temperature and Pollution'.format(state=states),
          'y':0.9, # new
          'x':0.5,
          'xanchor': 'center',
@@ -93,15 +98,75 @@ def display_graph(states):
         },
                        yaxis=dict(title='pm2.5',
                                    side='right'),
-                       yaxis2=dict(title='Temperature',
+                       yaxis2=dict(title='Temperature (°F)',
                                    overlaying='y',
                                    side='left'))
 
     return go.Figure(data=data, layout=layout)
 
 
+temp = '''@app.callback(
+    Output("AQI_Hist_line", "figure"), 
+    [Input("states", "value")])
+def historic_air(states):
+    """
+    Returns the historic air quality by state capital for the last year
+    """
+    df = aqi_hist[aqi_hist['state'].eq(states)]
+    fig = px.line(
+                x = df['date'], 
+                y = df[' pm25'],
+                labels={'date':'Date', 
+                ' pm25':'pm 2.5'}, 
+            )
+    fig.add_scatter(x=df['date'], y=df[' o3'], mode='lines', name = 'o3')
+    fig.add_scatter(x=df['date'], y=df[' pm10'], mode='lines', name = 'pm 10')
+    fig.add_scatter(x=df['date'], y=df[' no2'], mode='lines', name = 'no 2')
+    fig.add_scatter(x=df['date'], y=df[' so2'], mode='lines', name = 'so 2')
+    fig.add_scatter(x=df['date'], y=df[' co'], mode='lines', name = 'co')
+    fig.update_xaxes(title_text='Date')
+    fig.update_yaxes(title_text='pm 2.5')
+                              
+    return fig
+'''
 
+@app.callback(
+    Output("Weahter_forecast", "figure"), 
+    [Input("states", "value")])
+def weather_pre(states):
+    df = weather_pred[weather_pred['state'].eq(states)]
+    fig = px.line(
+        x = df['date_time'],
+        y = df['Temperature'],
+        #error_y = df['Maximum Temperature']
+        #error_y_minus= df['Minimum Temperature']
+    )
+    fig.update_xaxes(title_text='Date')
+    fig.update_yaxes(title_text='Temperture (°F)')
+    fig.update_layout(title={'text':'{state}\'s Temperature Forecast'.format(state=states),
+    'xanchor':'center',
+    'yanchor':'top',
+    'x':0.5})
+    return fig
 
+@app.callback(
+    Output("gen_metrics_temp", 'figure'), 
+    [Input("states", "value")])
+def general_metrics(states):
+    #daily_metrics = live.copy()
+    daily_metrics = live[live['state']==states]
+    daily_metrics['hour'] = daily_metrics['UTC_time'].str[:2]
+    daily_metrics['hour'] = daily_metrics['hour'].astype(str).astype(int)
+    daily_metrics = daily_metrics.loc[daily_metrics.groupby(['city', 'state'])['hour'].idxmax()]
+    daily_metrics = daily_metrics[['city', 'state','Temperature', 'feels_like','weather_description']]
+    
+    fig = go.Figure(go.Indicator(
+    mode = "number",
+    value = daily_metrics.Temperature.values[0],
+    number = {'suffix': " °F"},
+    domain = {'x': [0, 1], 'y': [0, 1]}))
+    fig.update_layout(paper_bgcolor = "lightgray")
+    return fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- data_manipulation.py
+++ data_manipulation.py
@@ -6,30 +6,33 @@ def import_data():
     #from datetime import date, timedelta, datetime
     import datetime as dt
 
-    today = datetime.today()
+    today = dt.date.today()
+    now = dt.datetime.today()
 
-    # Bring in the live data
+    # Bring in the live data that refresehes every hour on the 50th minute that captures the current floored hour:
     live = pd.read_csv('https://storage.googleapis.com/project-1050-data/live.csv')
+    # Bring in the state abbreviations for the map graph: 
     st_codes = pd.read_csv('https://storage.googleapis.com/project-1050-data/state_codes.csv')
-
     live = live.join(st_codes.set_index('State'), on="state")
     live.rename(columns={"temp":"Temperature", "Alpha code":"STATE"}, inplace=True)
 
+    # Adjust the time across 
     live['graph_date'] = live['UTC_date'] + ' '  + live['UTC_time']
-    live['UTC_date'] = pd.to_datetime(live['UTC_date']).dt.date()
+    live['UTC_date'] = pd.to_datetime(live['UTC_date']).dt.date
     dtm = today - dt.timedelta(1)
-    dtm = datetime.strptime(str(dtm), '%Y-%m-%d').date()
+    dtm = dt.datetime.strptime(str(dtm), '%Y-%m-%d').date()
     live = live[(live['UTC_date']>= dtm)]
 
-    historic = pd.read_csv('https://storage.cloud.google.com/project-1050-data/historic-air.csv?authuser=3')
+    # Bring in historical AQI data:
+    historic = pd.read_csv('https://storage.googleapis.com/project-1050-data/2021_AQI_hist.csv')
     historic['date'] =pd.to_datetime(historic.date)
-    
-    historic = historic[historic['date'] >= datetime(today.year, 1, 1)]
+    # makoing sure that no data before current year is brought in
+    historic = historic[historic['date'] >= dt.datetime(today.year, 1, 1)]
     historic = historic.sort_values(by=['date'])
-
-    weather_pred = pd.read_csv('https://storage.cloud.google.com/project-1050-data/weather_pred.csv?authuser=3')
+    # bring in the forecast for weatherr, whihc contains about 16 days worth of data (this dat get refreshed about once week)
+    weather_pred = pd.read_csv('https://storage.googleapis.com/project-1050-data/weather_pred.csv')
     weather_pred['date_time'] = pd.to_datetime(weather_pred['Date time'])
-    weather_pred = weather_pred[weather_pred['date_time'] >= today]
+    weather_pred = weather_pred[weather_pred['date_time'] >= now]
     weather_pred = weather_pred.sort_values(by=['date_time'])
 
     return live , historic , weather_pred
